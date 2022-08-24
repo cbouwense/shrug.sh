ff8a40658213aaf296ce57b1ee9f6789afda8813
Christian Bouwense <bouwensec@gmail.com>
2022-08-24 15:41:39 -0400

    copy pasted up to date oak

--- deno.ts
+++ deno.ts
@@ -1,9 +1,19 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-const app = new Application();
-
-app.use(({ response }) => {
-   response.body = "Hello World!";
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = `<!DOCTYPE html>
+    <html>
+      <head><title>Hello oak!</title><head>
+      <body>
+        <h1>Hello oak!</h1>
+      </body>
+    </html>
+  `;
 });
 
-addEventListener("fetch", app.fetchEventHandler());
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 8080 });
